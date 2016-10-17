112c69c163649f719660fe37d7a132dde6bf04ac
Andrew <as67@uowmail.edu.au>
2016-10-17 22:32:33 +1100

    Added declarations for update()

--- traffic.h
+++ traffic.h
@@ -12,10 +12,12 @@
 
 class Vehicle_Type{
     public:
+        Vehicle_Type();
         Vehicle_Type(std::string);              // Example: Vehicle_Type bus("Bus:0:LLLDDD:3:2:");
+        void update(std::string);               // Example: bus.update("Bus:0:LLLDDD:3:2:");
         void print(std::ostream&);              // Example: bus.print(cout);
         std::string get_name();                 // Example: string name = bus.getname();
-        std::string generate_plate();           // Example: string license = bus.generate_plate();
+        std::string generate_plate();           // Example: string registration = bus.generate_plate();
     private:
         std::string name;
         bool parking;
@@ -26,7 +28,9 @@ class Vehicle_Type{
 
 class Vehicle_Stats{
     public:
+        Vehicle_Stats();
         Vehicle_Stats(std::string);             // Example: Vehicle_Stats bus_stats("Bus:3:1:40:10:");
+        void update(std::string);               // Example: bus_stats.update("Bus:3:1:40:10:");
         void print(std::ostream&);              // Example: bus_stats.print(cout);
         std::string get_type();                 // Example: string type = bus_stats.gettype();
     private:
@@ -41,7 +45,7 @@ class Road_Stats{
     public:
         Road_Stats();
         Road_Stats(int, int, int);              // Example: Road_Stats elm_street(5, 60, 20);
-        void update(int, int, int);             // Example: elm_street.update(6, 50, 10);
+        void update(int, int, int);             // Example: elm_street.update(5, 60, 20);
         void print(std::ostream&);              // Example: elm_street.print(cout);
     private:
         int road_length;
