6ed85be522efa15a91e6f8e4393e427a9ed9cdc4
Andrew <as67@uowmail.edu.au>
2016-10-20 19:17:12 +1100

    Essentially complete, with some bugs

--- analysis.cpp
+++ analysis.cpp
@@ -4,6 +4,7 @@
  * Definitions for analysis engine.
  */
 
+#include <cmath>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -13,23 +14,19 @@
 #include "analysis.h"
 using namespace std;
 
-#define MAX_DAYS 100
-
 // analyzes log files and generates statistics
-//     statistics files are named day1.stats, day2.stats, and so on
 //     optional: passing 'true' as the final parameter generates base stats, which means filenames are base_daily.stats and base.stats
 //               the final parameter defaults to 'false', in which case the filenames default to live_daily.stats and live.stats
 //     if a file cannot be opened for writing, returns false
 bool perform_analysis(const vector<Vehicle_Type>& vehicles, int days, bool baseline)
 {
-    int event_counter[MAX_DAYS][5] = {{}};
-    string speeds[25][100] = {{""}};
-    int num_mean, num_sd, speed_mean, speed_sd;
+    Events_Data events_data;
+    vector<Vehicle_Data> vehicle_data;
     ifstream ifs;
     ofstream ofs;
 
     cout << "Log analysis in progress.\n";
-    int day, event, speed_index;
+    int day, event;
     string file, buffer, token, type;
     istringstream iss;
     // open the log file and read in relevant data
@@ -51,23 +48,25 @@ bool perform_analysis(const vector<Vehicle_Type>& vehicles, int days, bool basel
             // get event type token
             getline(iss, token, ':');
             event = atoi(token.c_str());
-            // update counter
-            event_counter[day-1][event-1]++;
-            // if it's a vehicle arrival, skip time, get vehicle name, get vehicle speed, store
+            // update counts
+            events_data.increment(day - 1, event - 1);
+            // if it's a vehicle arrival, skip time, get vehicle name, get vehicle speed, find object in vector, update data appropriately
             if(event == 1){
                 getline(iss, token, ':'); getline(iss, type, ':'); getline(iss, token, ':');
-                for(int i = 0; i < 25; i++){
-                    if(speeds[i][0] == ""){
-                        speeds[i][0] = type;
-                        speed_index = i;
-                        break;
-                    } else if(speeds[i][0] == type){
-                        speed_index = i;
+                int it = -1;
+                Vehicle_Data temp;
+                for(int i = 0; i < vehicle_data.size(); i++){
+                    if(vehicle_data[i].get_type() == type){
+                        it = i;
                         break;
                     }
                 }
-                speeds[speed_index][event_counter[day][event]+1] = token;
-                cout << speeds[speed_index][0] << ' ' << speeds[speed_index][event_counter[day][event]+1] << '\n';
+                if(it == -1){                          // not found so add a new one to vector
+                    temp.update(day, token, type);
+                    vehicle_data.push_back(temp);
+                } else {
+                    vehicle_data[it].update(day, token, type);
+                }
             }
         }
     } else {
@@ -83,18 +82,12 @@ bool perform_analysis(const vector<Vehicle_Type>& vehicles, int days, bool basel
     }
     ofs.open(file.c_str());
     if (ofs.is_open()){
-        for(int i = 0; i < days && ofs.good() == true; i++){
-            ofs << i + 1 << ':';
-            for(int j = 0; j < 5; j++)
-                ofs << event_counter[i][j] << ':';
-            ofs << '\n';
-        }
+        events_data.print(days, ofs);
     } else {
         cerr << "Unable to open file " << file << endl;
         return false;
     }
     ofs.close();
-    // perform analysis
     // write statistics to stat file
     if (baseline == true){
         file = "base.stats";
@@ -103,8 +96,8 @@ bool perform_analysis(const vector<Vehicle_Type>& vehicles, int days, bool basel
     }
     ofs.open(file.c_str());
     if (ofs.is_open()){
-        while(ofs.good() == true){
-            break; // code
+        for(int i = 0; i < vehicle_data.size() && ofs.good() == true; i++){
+            vehicle_data[i].print(days, ofs);
         }
     } else {
         cerr << "Unable to open file " << file << endl;
@@ -113,4 +106,106 @@ bool perform_analysis(const vector<Vehicle_Type>& vehicles, int days, bool basel
     cout << endl;
 
     return true;
+}
+
+/* Events_Data */
+
+// default constructor
+Events_Data::Events_Data()
+{
+    for(int i = 0; i < 100; i++){
+        for(int j = 0; j < 5; j++){
+            counts[i][j] = 0;
+        }
+    }
+}
+
+// increment a counter for a specific event on a given day
+void Events_Data::increment(int day, int event_num)
+{
+    counts[day][event_num]++;
+}
+
+// print events data
+void Events_Data::print(int days, ostream& out) const
+{
+    for(int i = 0; i < days; i++){
+        out << i + 1 << ':';
+        for(int j = 0; j < 5; j++){
+            out << counts[i][j] << ':';
+        }
+        out << '\n';
+    }
+}
+
+/* Vehicle_Data */
+
+// default constructor
+Vehicle_Data::Vehicle_Data()
+{
+    type = "";
+    num_mean = 0;
+    num_sd = 0;
+    speed_mean = 0;
+    speed_sd = 0;
+}
+
+void Vehicle_Data::update(int day, string speed, string vehicle_type)
+{
+    type = vehicle_type;
+    speeds[day].push_back(speed);
+}
+
+// print vehicle data
+void Vehicle_Data::print(int days, ostream& out)
+{
+    calculate(days);
+    out << type << ':' << num_mean << ':' << num_sd << ':' << speed_mean << ':' << speed_sd << ":\n";
+}
+
+// used for find function
+string Vehicle_Data::get_type() const
+{
+    return type;
+}
+
+// Updates means and SDs based on current data
+void Vehicle_Data::calculate(int days)
+{
+    vector<int> num_working, speeds_working;
+
+    // clear current values
+    num_mean = 0;
+    num_sd = 0;
+    speed_mean = 0;
+    speed_sd = 0;
+    // add all volumes and speeds to linear working buffers
+    for(int i = 0; i < days; i++){
+        num_working.push_back(speeds[i].size());
+        for(int j = 0; j < speeds[j].size(); j++){
+            speeds_working.push_back(atoi(speeds[i][j].c_str()));
+        }
+    }
+    // calculate means
+    for(int i = 0; i < num_working.size(); i++){
+        num_mean += num_working[i];
+    }
+    num_mean = num_mean / days;
+    for(int i = 0; i < speeds_working.size(); i++){
+        speed_mean += speeds_working[i];
+    }
+    speed_mean = speed_mean / days;
+    // calculate SDs
+    for(int i = 0; i < num_working.size(); i++){
+        num_sd += pow((num_working[i] - num_mean), 2);
+    }
+    num_sd = num_sd / days;
+    num_sd = sqrt(num_sd);
+    for(int i = 0; i < speeds_working.size(); i++){
+        speed_sd += pow((speeds_working[i] - speed_mean), 2);
+    }
+    speed_sd = speed_sd / days;
+    speed_sd = sqrt(speed_sd);
+
+    return;
 }
--- analysis.h
+++ analysis.h
@@ -7,8 +7,36 @@
 #ifndef ANALYSIS_H
 #define ANALYSIS_H
 
+#include <iostream>
 #include <vector>
 #include "traffic.h"
+
+/* Using '100' based on MAX_DAYS in main.cpp - need to find a better solution */
+
+class Events_Data{
+    public:
+        Events_Data();
+        void increment(int, int);                                 // Example: events_data.update(x, y, 50);
+        void print(int, std::ostream&) const;                     // Example: events_data.print(5, ofs);
+    private:
+        int counts[100][5];                                       // Stores event counter by day
+};
+
+class Vehicle_Data{
+    public:
+        Vehicle_Data();
+        void update(int, std::string, std::string);               // Example: vehicle_data.update(2, "22", "Emergency");
+        void print(int, std::ostream&);                           // Example: vehicle_data.print(5, ofs);
+        std::string get_type() const;                             // Example: string type = bus.get_type();
+    private:
+        void calculate(int);                                      // Updates means and SDs based on current data
+        std::string type;
+        std::vector<std::string> speeds[100];                     // Stores speeds by day
+        int num_mean;
+        int num_sd;
+        int speed_mean;
+        int speed_sd;
+};
 
 bool perform_analysis(const std::vector<Vehicle_Type>&, int days, bool baseline = false);
 
